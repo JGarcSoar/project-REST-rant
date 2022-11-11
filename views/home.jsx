d760d84c05aaee4f69291a253462a1297f88bb36
JGarcSoar <garciasoares.joe@gmail.com>
2022-11-10 21:49:02 -0800

    adding bootstrap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,24 @@ function home() {
     <Def>
       <main>
         <h1>HOME</h1>
+        <div>
+          <img
+            src="../images/roasted-table.jpg"
+            alt="Table with roasted meats and vegetables"
+            width="400"
+            height="500"
+          />
+          <div>
+            Photo by{" "}
+            <a href="https://unsplash.com/@victoriakosmo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Victoria Shes
+            </a>{" "}
+            on{" "}
+            <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Unsplash
+            </a>
+          </div>
+        </div>
         <a href="/places">
           <button className="btn-primary">Places Page</button>
         </a>
@@ -14,4 +32,4 @@ function home() {
   );
 }
 
-module.exports = home;
+https: module.exports = home;
