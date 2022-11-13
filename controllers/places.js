12227d493ef4d2b12445286e757383e159230c8e
JGarcSoar <garciasoares.joe@gmail.com>
2022-11-13 11:25:04 -0800

    added a lot functionality

--- controllers/places.js
+++ controllers/places.js
@@ -1,28 +1,29 @@
 const router = require("express").Router();
+const places = require("../models/places.js");
 
 // GET /places
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "../images/thai-food.jpg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "../images/kittens.jpg",
-    },
-  ];
   res.render("places/index", { places });
 });
 
 router.get("/new", (req, res) => {
-  res.render("places/new");
+  res.render("places/new", { places });
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
 });
 
 module.exports = router;
